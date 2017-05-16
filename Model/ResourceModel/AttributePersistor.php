f33d68aac9beaad513c082dc96e4a11abbe9e040
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2017-05-16 10:41:49 +0200

    Scoped EAV attribute persistor.

---
+++ Model/ResourceModel/AttributePersistor.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\ScopedEav
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ScopedEav\Model\ResourceModel;
+
+use Magento\Framework\Model\Entity\ScopeInterface;
+use Magento\Eav\Model\Entity\Attribute\AbstractAttribute;
+use Smile\ScopedEav\Api\Data\AttributeInterface as ScopedEavAttribute;
+
+
+/**
+ * Scoped EAV entity attribute persistor.
+ *
+ * @category Smile
+ * @package  Smile\ScopedEav
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class AttributePersistor extends \Magento\Eav\Model\ResourceModel\AttributePersistor
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+     */
+    protected function getScopeValue(ScopeInterface $scope, AbstractAttribute $attribute, $useDefault = false)
+    {
+        if ($attribute instanceof ScopedEavAttribute) {
+            $useDefault = $useDefault || $attribute->isScopeGlobal();
+        }
+
+        return parent::getScopeValue($scope, $attribute, $useDefault);
+    }
+}
