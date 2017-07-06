dc2325ccd4975fdc8fa016482dfb6d53e040097f
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2017-07-06 14:09:42 +0200

    Append UI Components EAV attrbute validation rules.

---
+++ Ui/DataProvider/Entity/Form/EavValidationRules.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+* versions in the future.
+*
+*
+* @category  Smile
+* @package   Smile\ScopedEav
+* @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+* @copyright 2016 Smile
+* @license   Open Software License ("OSL") v. 3.0
+*/
+
+namespace Smile\ScopedEav\Ui\DataProvider\Entity\Form;
+
+use \Smile\ScopedEav\Api\Data\AttributeInterface;
+
+/**
+ * Scoped EAV form validation rules.
+ *
+ * @category Smile
+ * @package  Smile\ScopedEav
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class EavValidationRules
+{
+    /**
+     * Build validation rules for the attribute.
+     *
+     * @param ProductAttributeInterface $attribute Attribute.
+     * @param array                     $data      Additional data.
+     *
+     * @return array
+     */
+    public function build(AttributeInterface $attribute, array $data)
+    {
+        $rules = [];
+
+        if (! empty($data['required'])) {
+            $rules['required-entry'] = true;
+        }
+
+        $validationClasses = explode(' ', $attribute->getFrontendClass());
+
+        foreach ($validationClasses as $class) {
+            if (preg_match('/^maximum-length-(\d+)$/', $class, $matches)) {
+                $rules = array_merge($rules, ['max_text_length' => $matches[1]]);
+                continue;
+            }
+            if (preg_match('/^minimum-length-(\d+)$/', $class, $matches)) {
+                $rules = array_merge($rules, ['min_text_length' => $matches[1]]);
+                continue;
+            }
+
+            $rules = $this->mapRules($class, $rules);
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Map fontend classes with rules.
+     *
+     * @param string $class Frontend class.
+     * @param array  $rules Validation rules.
+     *
+     * @return array
+     */
+    private function mapRules($class, array $rules)
+    {
+        switch ($class) {
+            case 'validate-number':
+            case 'validate-digits':
+            case 'validate-email':
+            case 'validate-url':
+            case 'validate-alpha':
+            case 'validate-alphanum':
+                $rules = array_merge($rules, [$class => true]);
+                break;
+        }
+
+        return $rules;
+    }
+}
